6df1425d4f2bad6a90acffb665781447dc4a9b42
Rapid-Dev <johay.jjj@googlemail.com>
2015-05-17 04:29:42 +0100

    Update - Register
    - Started on backend (added vars, added empty check)

--- login.php
+++ login.php
@@ -24,19 +24,22 @@
 				<b>Already have an account?</b><br />
 					<input type="text" name="username" placeholder="Type your username">
 					<input type="text" name="password" placeholder="Type your password">
-					<input type="submit" class="login" value="Submit">
+					<input type="submit" name="loginsubmit" class="login" value="Submit">
 
 			</div>
 
 			<div id="content-registerform">
 				<b>Already have an account?</b><br />
-					<input type="text" name="username" placeholder="First Name">
-					<input type="text" name="username" placeholder="Second Name">
-					<input type="text" name="username" placeholder="Username">
-					<input type="text" name="password" placeholder="Password">
-					<input type="text" name="password" placeholder="E-Mail">
-					<input type="text" name="password" placeholder="Verify E-Mail">
-					<p style="text-align: center;"><input type="submit" class="login" value="Submit"></p>
+					<form name="regform" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+						<input type="text" name="fstname"  placeholder="First Name">
+						<input type="text" name="lstname"  placeholder="Surname">
+						<input type="text" name="usrname"  placeholder="Username">
+						<input type="text" name="psword"   placeholder="Password">
+						<input type="text" name="vfypsword" placeholder="Verify Password">
+						<input type="text" name="email"    placeholder="E-Mail">
+						<p style="text-align: center;">
+						<input name="regbtn" type="submit" class="login" value="Submit"></p>
+					</form>
 
 			</div><div style="clear:both"></div>
 
@@ -67,3 +70,26 @@
 
 	</body>
 </html>
+
+<?php
+error_reporting(E_ERROR);
+	if (isset($_POST['regbtn'])){
+		// register form handling
+		$fname = $_POST['fstname'];
+		$lname = $_POST['lstname'];
+		$usrname = $_POST['usrname'];
+		$psword = $_POST['psword'];
+		$psword2 = $_POST['vfypsword'];
+		$email = $_POST['email'];
+
+			if (!empty($fname) && !empty($lname) && !empty($usrname) && !empty($psword) && !empty($psword2) && !empty($email)){
+				echo "Thank for submitting your information." . $fname;
+			}else{
+				echo "Please fill out all the fields.";
+			}
+
+	}
+
+
+
+?>
