de217879bedda1014d9a6f2d4ea835d1e00498f5
Rapid-Dev <johay.jjj@googlemail.com>
2015-05-18 00:22:31 +0100

    Update Register
    - Finished registration by adding same username and email checks
    - Final register refactor of code

--- login.php
+++ login.php
@@ -1,97 +1,116 @@
 <?php
 error_reporting(E_ERROR);
 
-	if (isset($_POST['regbtn'])){
-		// register form handling
-		$fname = $_POST['fstname'];
-		$lname = $_POST['lstname'];
-		$usrname = $_POST['usrname'];
-		$psword = $_POST['psword'];
-		$psword2 = $_POST['vfypsword'];
-		$email = $_POST['email'];
-		include("db/config.php");
-		$conn = dbh();
+// -- REGISTER SECTION --
+if (isset($_POST['regbtn'])){
+	$fname = $_POST['fstname'];
+	$lname = $_POST['lstname'];
+	$usrname = $_POST['usrname'];
+	$psword = $_POST['psword'];
+	$psword2 = $_POST['vfypsword'];
+	$email = $_POST['email'];
+	include("db/config.php");
+	$conn = dbh();
 
-			if (!empty($fname) && !empty($lname) && !empty($usrname) && !empty($psword) && !empty($psword2) && !empty($email) && $psword == $psword2 && filter_var($email, FILTER_VALIDATE_EMAIL)){
+	// database checks for email
+	$sth = $conn->prepare("SELECT COUNT(*) FROM users WHERE email = :email");
+	$sth->bindParam(':email', $email);
+	$sth->execute();
+	$email_rows = $sth->fetchColumn(); 
 
-					$rOutput = "Thank for submitting your information.";
-					// Info is correct, password hashing now
-					$options = [
-						'cost' => 11,
-						'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM),
-					];
-					$hashed_password = password_hash($psword, PASSWORD_DEFAULT, $options);
-					// add data to db
-					$stmt = $conn->prepare("INSERT INTO users (firstname, lastname, username, password, email) VALUES (:fname, :lname, :usrname, :hashed_password, :email)");
-					$stmt->bindParam(':fname', $fname);
-				    $stmt->bindParam(':lname', $lname);
-					$stmt->bindParam(':usrname', $usrname);
-					$stmt->bindParam(':hashed_password', $hashed_password);
-					$stmt->bindParam(':email', $email);
-					$stmt->execute();
+	// database checks for username
+	$sth = $conn->prepare("SELECT COUNT(*) FROM users WHERE username = :usrname");
+	$sth->bindParam(':usrname', $usrname);
+	$sth->execute();
+	$username_rows = $sth->fetchColumn(); 
 
-			}elseif ($psword != $psword2){
+	// start of conditional statements
+	if (!empty($fname) && !empty($lname) && !empty($usrname) && !empty($psword) && !empty($psword2) && !empty($email) && $psword == $psword2 && filter_var($email, FILTER_VALIDATE_EMAIL) && $email_rows == 0 && $username_rows == 0){
 
-				$rOutput = "Passwords do not match!";
+		$rOutput = "Thank for submitting your information.";
 
-				// check email, username, dont exist
+		// Info is correct, password hashing now
+		$options = [
+		'cost' => 11,
+		'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM),
+		];
+		$hashed_password = password_hash($psword, PASSWORD_DEFAULT, $options);
 
-			}else{
+		// add data to db
+		$stmt = $conn->prepare("INSERT INTO users (firstname, lastname, username, password, email) VALUES (:fname, :lname, :usrname, :hashed_password, :email)");
+		$stmt->bindParam(':fname', $fname);
+		$stmt->bindParam(':lname', $lname);
+		$stmt->bindParam(':usrname', $usrname);
+		$stmt->bindParam(':hashed_password', $hashed_password);
+		$stmt->bindParam(':email', $email);
+		$stmt->execute();
 
-				$rOutput = "Please fill out all the fields correctly.";
-			}
+	}elseif ($psword != $psword2){
 
+		$rOutput = "Passwords do not match!";
+
+	}else if ($email_rows >= 1){
+
+		$rOutput = "This e-mail address is already registered.";
+
+	}else if ($username_rows >= 1){
+
+		$rOutput = "This username is already registered.";
+
+	}else{
+
+		$rOutput = "Please fill out all the fields correctly.";
 	}
 
-
+}
 
 ?>
 
 <html>
-	<body>
-		<head>
-			<Title>Smart Store - Made by Jonathan Mummery</Title>
-			<link rel="Stylesheet" href="css/style.css">
-		</head>
+<body>
+	<head>
+		<Title>Smart Store - Made by Jonathan Mummery</Title>
+		<link rel="Stylesheet" href="css/style.css">
+	</head>
 
-		<div id="wrapper">
+	<div id="wrapper">
 
-			<div id="header">
-					<ul>
-						<li><a class="navlinks" href="#">Store</a></li>
-						<li><a class="navlinks" href="#">Account</a></li>
-						<li><a class="navlinks" href="#">Contact</a></li>
-						<li><a class="navlinks" href="#">Offers</a></li>
-						<li><a class="navlinks" href="login.php">Login</a></li>
-						<li><a class="aboutlink" href="index.php">About</a></li>
-						<p class="top">This website is not mobile friendly.</p>
-					</ul>
+		<div id="header">
+			<ul>
+				<li><a class="navlinks" href="#">Store</a></li>
+				<li><a class="navlinks" href="#">Account</a></li>
+				<li><a class="navlinks" href="#">Contact</a></li>
+				<li><a class="navlinks" href="#">Offers</a></li>
+				<li><a class="navlinks" href="login.php">Login</a></li>
+				<li><a class="aboutlink" href="index.php">About</a></li>
+				<p class="top">This website is not mobile friendly.</p>
+			</ul>
 
-			</div>
+		</div>
 
-			<div id="content-loginform">
-				<b>Already have an account?</b><br />
-					<input type="text" name="username" placeholder="Type your username">
-					<input type="text" name="password" placeholder="Type your password">
-					<input type="submit" name="loginsubmit" class="login" value="Submit">
+		<div id="content-loginform">
+			<b>Already have an account?</b><br />
+			<input type="text" name="username" placeholder="Type your username">
+			<input type="text" name="password" placeholder="Type your password">
+			<input type="submit" name="loginsubmit" class="login" value="Submit">
 
-			</div>
+		</div>
 
-			<div id="content-registerform">
-				<?php 	
-					if (!empty($rOutput)) { echo "<center><b>" . $rOutput . "</b></center>"; }
-					else {echo "<b>Don't have an acocunt? Sign up!</b><br />";} 
-				?>
-					<form name="regform" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-						<input type="text" name="fstname"  placeholder="First Name">
-						<input type="text" name="lstname"  placeholder="Surname">
-						<input type="text" name="usrname"  placeholder="Username">
-						<input type="password" name="psword"   placeholder="Password">
-						<input type="password" name="vfypsword" placeholder="Verify Password">
-						<input type="text" name="email"    placeholder="E-Mail">
-						<p style="text-align: center;">
-						<input name="regbtn" type="submit" class="login" value="Submit"></p>
-					</form>
+		<div id="content-registerform">
+			<?php 	
+			if (!empty($rOutput)) { echo "<center><b>" . $rOutput . "</b></center>"; }
+			else {echo "<b>Don't have an acocunt? Sign up!</b><br />";} 
+			?>
+			<form name="regform" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+				<input type="text" name="fstname"  placeholder="First Name">
+				<input type="text" name="lstname"  placeholder="Surname">
+				<input type="text" name="usrname"  placeholder="Username">
+				<input type="password" name="psword"   placeholder="Password">
+				<input type="password" name="vfypsword" placeholder="Verify Password">
+				<input type="text" name="email"    placeholder="E-Mail">
+				<p style="text-align: center;">
+					<input name="regbtn" type="submit" class="login" value="Submit"></p>
+				</form>
 
 			</div><div style="clear:both"></div>
 
@@ -104,21 +123,21 @@ error_reporting(E_ERROR);
 				</div>
 				<div id="footer-content-links">
 					<ul class="footer">
-					<li class="footer"><a class="footerlinks" href="#">Contact</a></li>
-					<li class="footer"><a class="footerlinks" href="#">Private Policy</a></li>
-					<li class="footer"><a class="footerlinks" href="#">Terms of Service</a></li>
-					<li class="footer"><a class="footerlinks" href="#">Copyright Notice</a></li>
-					<li class="footer"><a class="footerlinks" href="#">FAQs</a></li>
-					<li class="footer"><a class="footerlinks" href="#">Deliverys</a></li>
-					<li class="footer"><a class="footerlinks" href="#">Warranty</a></li>
-					<li class="footer"><a class="footerlinks" href="#">Support</a></li>
-					<li class="footer"><a class="footerlinks" href="#">About Us</a></li>
-					<ul>
+						<li class="footer"><a class="footerlinks" href="#">Contact</a></li>
+						<li class="footer"><a class="footerlinks" href="#">Private Policy</a></li>
+						<li class="footer"><a class="footerlinks" href="#">Terms of Service</a></li>
+						<li class="footer"><a class="footerlinks" href="#">Copyright Notice</a></li>
+						<li class="footer"><a class="footerlinks" href="#">FAQs</a></li>
+						<li class="footer"><a class="footerlinks" href="#">Deliverys</a></li>
+						<li class="footer"><a class="footerlinks" href="#">Warranty</a></li>
+						<li class="footer"><a class="footerlinks" href="#">Support</a></li>
+						<li class="footer"><a class="footerlinks" href="#">About Us</a></li>
+						<ul>
+						</div>
+
+					</div>
+
 				</div>
 
-			</div>
-
-		</div>
-
-	</body>
-</html>
+			</body>
+			</html>
